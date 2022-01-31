8a4de70c006421ecf54be27bc8616e67be0092d6
abidi <safa.abidi99@gmail.com>
2022-01-31 20:52:58 +0100

    register page

---
+++ android/app/src/main/kotlin/com/safa/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.safa.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+        //elevation: 0,
+      ),
+      body: Column(
+        children: [
+          TextField(),
+          TextField(),
+          TextButton(
+            child: Text('Register'),
+            onPressed: () async {},
+          ),
+        ],
+      ),
+    );
+  }
+}
